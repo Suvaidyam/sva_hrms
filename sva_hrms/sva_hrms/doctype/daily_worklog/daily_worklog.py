eaef5b29f3650dd58079276f9af08f10b8491a28
vrindarak vishwakarma <vrindarak.vishwakarma@dhwaniris.com>
2025-06-07 17:56:54 +0530

    Change the daily worklog

--- sva_hrms/sva_hrms/doctype/daily_worklog/daily_worklog.py
+++ sva_hrms/sva_hrms/doctype/daily_worklog/daily_worklog.py
@@ -7,11 +7,29 @@ from frappe.model.document import Document
 
 class DailyWorklog(Document):
     def after_insert(self):
-        time_sheet_doc = frappe.new_doc('Timesheet')
-        time_sheet_doc.employee = self.employee
-        time_sheet_doc.custom_timesheet_date = self.date
+        self.create_or_update_timesheet()
+
+    def on_update(self):
+        self.create_or_update_timesheet()
+
+    def create_or_update_timesheet(self):
+        # Check if a Timesheet already exists for this DailyWorklog
+        existing_timesheet = frappe.db.exists(
+            'Timesheet', {'employee': self.employee, 'custom_timesheet_date': self.date}
+        )
         
-        if self.timesheet_detail:  # Check if the child table has data
+        if existing_timesheet:
+            # Fetch the existing Timesheet document
+            time_sheet_doc = frappe.get_doc('Timesheet', existing_timesheet)
+            time_sheet_doc.time_logs = []  # Clear existing time logs
+        else:
+            # Create a new Timesheet document
+            time_sheet_doc = frappe.new_doc('Timesheet')
+            time_sheet_doc.employee = self.employee
+            time_sheet_doc.custom_timesheet_date = self.date
+        
+        # Populate the time logs from the DailyWorklog
+        if self.timesheet_detail:
             for data in self.timesheet_detail:
                 time_sheet_doc.append('time_logs', {
                     'project': data.project,
@@ -20,7 +38,8 @@ class DailyWorklog(Document):
                     'description': data.description,
                     'from_time': data.from_time,
                     'to_time': data.to_time,
-                    'is_billable': data.is_billable, 
+                    'is_billable': data.is_billable,
                 })
-	
-        time_sheet_doc.insert(ignore_permissions=True)
+
+        # Save or update the Timesheet document
+        time_sheet_doc.save(ignore_permissions=True)
